2e1cb15cd8ef7d762c3ee952a27ad966bd63dd89
Frederic Abraham <mail@fabraham.dev>
2025-06-20 01:08:11 +0200

    feat: add @nuxthub/core module to Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxtjs/color-mode",
     "@pinia/nuxt",
+    "@nuxthub/core",
   ],
 
   // Add content configuration for Cloudflare Pages
